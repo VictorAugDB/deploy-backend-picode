e41dc497d5bd2a3aa3a41ecf6d5a4f43deec244f
Victor Augusto <vdbernardes@gmail.com>
2021-02-26 19:07:38 -0300

    deploy-backend-picode

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import 'reflect-metadata';
+
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+import './database';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('💥 server started on port 3333! 💥');
+});
---
+++ src/services/FindByTag.ts
@@ -0,0 +1,28 @@
+import { getRepository } from 'typeorm';
+import Book from '../models/Book';
+
+interface Request {
+  tag: string[];
+}
+
+class CreateBookService {
+  public async execute({
+    tag,
+  }: Request): Promise<Book[] | undefined> {
+    const booksRepository = getRepository(Book);
+
+    const books = await booksRepository.find();
+
+    const tags = books.filter(
+      (book) => {
+        const array = JSON.parse(JSON.stringify(book.tags)).toLowerCase();
+
+        return array.split(/\W+/).includes(tag);
+      },
+    );
+
+    return tags;
+  }
+}
+
+export default CreateBookService;
